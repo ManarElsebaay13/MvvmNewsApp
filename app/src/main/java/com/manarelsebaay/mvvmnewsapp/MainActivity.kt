92586e870c945c46ed9efc096674f2f0001d3cdb
manarelsebaay <manarelsebaay13@gmail.com>
2022-08-02 21:21:39 +0200

    ts

--- app/src/main/java/com/manarelsebaay/mvvmnewsapp/MainActivity.kt
+++ app/src/main/java/com/manarelsebaay/mvvmnewsapp/MainActivity.kt
@@ -11,7 +11,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         bottomNavigationView.setupWithNavController(nav_graph_host.findNavController())
     }
 }
+
+//mmmmm
